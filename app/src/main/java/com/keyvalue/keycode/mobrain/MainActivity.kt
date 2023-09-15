f88e14df2a2d73f994ef7dc6c4f2c9c3995b03af
devkishan <devkishan@keyvalue.systems>
2023-09-15 09:38:11 +0530

    feat: Add android boiler plate code

---
+++ app/src/main/java/com/keyvalue/keycode/mobrain/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.keyvalue.keycode.mobrain
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.keyvalue.keycode.mobrain.ui.theme.MoBrainTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoBrainTheme {
+
+            }
+        }
+    }
+}
+
